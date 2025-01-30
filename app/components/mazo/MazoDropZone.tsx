53bdb78c83b8c0fed2517a625ba410906c811b50
Leandro Beretta <lea.beretta@gmail.com>
2025-01-30 17:08:11 +0000

    drop zone

--- app/components/mazo/MazoDropZone.tsx
+++ app/components/mazo/MazoDropZone.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 export default function MazoDropZone({ onUpload }: { onUpload: (files: File[]) => void }) {
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
