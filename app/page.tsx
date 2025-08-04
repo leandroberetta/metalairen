964fe0aed707329bd2735d1a5e2db28e51e721c0
Leandro Beretta <lea.beretta@gmail.com>
2025-08-04 22:01:46 +0000

    spoiler

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,24 @@ export default async function Cartas() {
       <Suspense fallback={<LoadingSpinner />}>
         <CartaHeader cartas={cartas} />
       </Suspense >
+      <div className="relative bg-[#111827] mt-5 mb-5">
+        {/* Capa de fondo degradado que ocupa todo el ancho y altura */}
+        <div className="absolute inset-0 z-0">
+          <div className="w-full h-full bg-gradient-to-br from-[#641717] via-[#5f2a14] to-[#7d4a11] opacity-50 blur-2xl" />
+        </div>
+
+        {/* Contenido centrado con carta y borde */}
+        <div className="relative z-10 flex items-center justify-center ">
+          {/* Aura más cercana a la carta */}
+          <div className="relative p-4">
+            <img
+              src="/yoro.png"
+              alt="Yorō, leyenda del fin"
+              className="relative z-10 w-[280px] sm:w-[320px] md:w-[360px] rounded-xl"
+            />
+          </div>
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
         <div>
           <h1 className="mb-4 text-xl md:text-3xl font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-3xl"><span
